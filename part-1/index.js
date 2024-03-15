cedeee796937998b36d3d81b2104b5392fd97965
Matthew Ozuna <mattoz@matthews-mbp.mynetworksettings.com>
2024-03-15 19:31:44 -0400

    completed part 1

---
+++ part-1/index.js
@@ -0,0 +1,9 @@
+const App = () => (
+        <div>
+            <FirstComponent />
+            <NamedComponent name="Matt"/>
+        </div>
+    )
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
